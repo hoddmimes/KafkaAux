983322bdcb81f8c103b4a9a66f4849cf5fc633a2
Hoddmimes <par.bertilsson@yahoo.com>
2020-03-03 09:34:47 +0100

    Fixed ObjectMapper

--- src/main/java/com/hoddmimes/kafka/SimpleJsonMapper.java
+++ src/main/java/com/hoddmimes/kafka/SimpleJsonMapper.java
@@ -4,41 +4,36 @@ package com.hoddmimes.kafka;
 import com.google.gson.*;
 import com.google.gson.internal.LinkedHashTreeMap;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SimpleJsonMapper
+public class SimpleJsonMapper extends LinkedHashMap<String,Object>
 {
-    private LinkedHashMap<String,Object> mMap;
 
     public SimpleJsonMapper( LinkedHashMap<String,Object> pMap ) {
-        if (pMap == null) {
-            mMap = null;
-        } else {
-            mMap = copyMap(pMap);
-        }
+        super();
+        this.putAll( copyMap( pMap ) );
     }
 
     public SimpleJsonMapper(  JsonObject pJsonObject ) {
-        if (pJsonObject == null) {
-            mMap = new LinkedHashMap<>();
-        } else {
-            mMap = unpackJson(pJsonObject);
-        }
+        super();
+        this.putAll(unpackJson(pJsonObject));
     }
 
     private LinkedHashMap<String,Object> unpackJson( JsonObject pJsonObject ) {
         LinkedHashMap<String,Object> tMap = new LinkedHashMap<>();
         for( String pKey : pJsonObject.keySet()) {
            JsonElement jElement = pJsonObject.get( pKey );
-           mMap.put( pKey, parseJsonObject( jElement));
+           tMap.put( pKey, parseJsonObject( jElement));
         }
         return tMap;
     }
 
     private Object parseJsonObject( JsonElement jElement ) {
+        // Array ?
         if (jElement.isJsonArray()) {
             List<Object> tList = new ArrayList<>();
             JsonArray jArray = jElement.getAsJsonArray();
@@ -47,22 +42,75 @@ public class SimpleJsonMapper
             }
             return tList;
         }
+        // Object ?
         if (jElement.isJsonObject()) {
             return unpackJson( jElement.getAsJsonObject());
         }
+        // Primitive ?
         if (jElement.isJsonPrimitive()) {
            JsonPrimitive jPrimitive = jElement.getAsJsonPrimitive();
            if (jPrimitive.isBoolean()) {
                return jPrimitive.getAsBoolean();
            }
+            if (jPrimitive.isNumber()) {
+                return parseJsonNumber( jPrimitive );
+            }
            if (jPrimitive.isString()) {
                return jPrimitive.getAsString();
            }
-           if (jPrimitive.isNumber()) {
-              return null;
-           }
         }
         return null;
+    }
+
+    public Map<String, Object> asMap() {
+        return this;
+    }
+
+    private JsonElement toJsonPrimitive( Object pObject ) {
+        if (pObject instanceof Boolean) {
+            return new JsonPrimitive( (Boolean) pObject );
+        }
+        if (pObject instanceof Double) {
+            return new JsonPrimitive( (Double) pObject );
+        }
+        if (pObject instanceof Long) {
+            return new JsonPrimitive( (Long) pObject );
+        }
+        if (pObject instanceof Integer) {
+            return new JsonPrimitive( (Integer) pObject );
+        }
+        return new JsonPrimitive( (String) pObject.toString());
+    }
+
+    private JsonPrimitive toJsonArray( List<Object> pList) {
+        JsonArray jArray = new JsonArray();
+        for( Object o : pList ) {
+           jArray.add( toJsonElement( 0));
+        }
+        return jArray.getAsJsonPrimitive();
+    }
+
+    public  JsonObject asJson() {
+        return toJsonObject( this );
+    }
+
+    private  JsonElement toJsonElement( Object pObject  ) {
+        if (pObject instanceof List) {
+            return toJsonArray((List) pObject);
+        }
+        if (pObject instanceof Map) {
+            return toJsonObject((Map) pObject);
+        }
+        return toJsonPrimitive(pObject);
+    }
+
+    private  JsonObject toJsonObject( Map<String,Object> pMap )
+    {
+        JsonObject jObj = new JsonObject();
+        for( Map.Entry<String,Object> tItem : pMap.entrySet() ) {
+           jObj.add(  tItem.getKey(), toJsonElement(tItem.getValue()));
+        }
+        return jObj;
     }
 
 
@@ -82,27 +130,37 @@ public class SimpleJsonMapper
         return tMap;
     }
 
+
+    public static Object parseJsonNumber( JsonPrimitive jNumberPrimitive ) {
+        String jNumberStr = jNumberPrimitive.getAsString();
+        // Double ?
+        if (jNumberStr.contains(".")) {
+            return Double.parseDouble(jNumberStr);
+        }
+        // HEX string ?
+        if (jNumberStr.toLowerCase().startsWith("0x")) {
+            try {
+                return Integer.parseInt(jNumberStr, 16);
+            } catch (NumberFormatException e) {
+                return Long.parseLong(jNumberStr, 16);
+            }
+        }
+        // Plain integer or Long
+        try {
+            return Integer.parseInt(jNumberStr);
+        } catch (NumberFormatException e) {
+            return Long.parseLong(jNumberStr);
+        }
+    }
+
+
+
+
     public static void main(String[] args ) {
         String js = "{\"int\" : 123, \"float\": 3.14, \"long\" :1234567891234, \"string\" : \"hej hopp string\",\"boolean\" : true }";
         JsonObject jObj = new JsonParser().parse( js ).getAsJsonObject();
-        for(String k : jObj.keySet()) {
+            JsonPrimitive p = jObj.get("long").getAsJsonPrimitive();
+            Object o = parseJsonNumber( p );
 
-            Object o = jObj.get(k);
-            if (o instanceof Integer) {
-                System.out.println("Integer");
-            }
-            if (o instanceof Long) {
-                System.out.println("Long");
-            }
-            if (o instanceof String) {
-                System.out.println("String");
-            }
-            if (o instanceof Boolean) {
-                System.out.println("Boolean");
-            }
-            if (o instanceof Double) {
-                System.out.println("Boolean");
-            }
-        }
     }
 }
